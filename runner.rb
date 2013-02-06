34e9d47452f9b5180416c8f69559b3b9bf2e7215
Attila Domokos <adomokos@gmail.com>
2013-02-05 22:29:26 -0600

    Adding a runner, that reads in menu files

---
+++ runner.rb
@@ -0,0 +1,17 @@
+require_relative 'lib/xkcd'
+
+Dir.glob("menus/*.txt") do |menu_file|
+  file = File.open(menu_file, "rb")
+  contents = file.read
+
+  context = Xkcd::SelectsMenus.from_input_string(contents)
+
+  if context.failure?
+    puts "#{context.message} in file: #{menu_file}"
+  else
+    puts "Menu from file: #{menu_file}"
+    puts context[:result]
+  end
+
+  puts "======================"
+end
